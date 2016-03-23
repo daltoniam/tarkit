2bc2873150f624d6275132ae2ba37af561ea0c52
Patrice Brend'amour <patrice@brendamour.net>
2016-03-23 19:46:53 +0100

    implemented chunked writing for tar files

--- DCTar.m
+++ DCTar.m
@@ -69,48 +69,48 @@
 
 
 static const char template_header[] = {
-	/* name: 100 bytes */
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,
-	/* Mode, space-null termination: 8 bytes */
-	'0','0','0','0','0','0', ' ','\0',
-	/* uid, space-null termination: 8 bytes */
-	'0','0','0','0','0','0', ' ','\0',
-	/* gid, space-null termination: 8 bytes */
-	'0','0','0','0','0','0', ' ','\0',
-	/* size, space termation: 12 bytes */
-	'0','0','0','0','0','0','0','0','0','0','0', ' ',
-	/* mtime, space termation: 12 bytes */
-	'0','0','0','0','0','0','0','0','0','0','0', ' ',
-	/* Initial checksum value: 8 spaces */
-	' ',' ',' ',' ',' ',' ',' ',' ',
-	/* Typeflag: 1 byte */
-	'0',			/* '0' = regular file */
-	/* Linkname: 100 bytes */
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,
-	/* Magic: 6 bytes, Version: 2 bytes */
-	'u','s','t','a','r','\0', '0','0',
-	/* Uname: 32 bytes */
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	/* Gname: 32 bytes */
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	/* rdevmajor + space/null padding: 8 bytes */
-	'0','0','0','0','0','0', ' ','\0',
-	/* rdevminor + space/null padding: 8 bytes */
-	'0','0','0','0','0','0', ' ','\0',
-	/* Prefix: 155 bytes */
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,
-	/* Padding: 12 bytes */
-	0,0,0,0,0,0,0,0, 0,0,0,0
+    /* name: 100 bytes */
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,
+    /* Mode, space-null termination: 8 bytes */
+    '0','0','0','0','0','0', ' ','\0',
+    /* uid, space-null termination: 8 bytes */
+    '0','0','0','0','0','0', ' ','\0',
+    /* gid, space-null termination: 8 bytes */
+    '0','0','0','0','0','0', ' ','\0',
+    /* size, space termation: 12 bytes */
+    '0','0','0','0','0','0','0','0','0','0','0', ' ',
+    /* mtime, space termation: 12 bytes */
+    '0','0','0','0','0','0','0','0','0','0','0', ' ',
+    /* Initial checksum value: 8 spaces */
+    ' ',' ',' ',' ',' ',' ',' ',' ',
+    /* Typeflag: 1 byte */
+    '0',			/* '0' = regular file */
+    /* Linkname: 100 bytes */
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,
+    /* Magic: 6 bytes, Version: 2 bytes */
+    'u','s','t','a','r','\0', '0','0',
+    /* Uname: 32 bytes */
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    /* Gname: 32 bytes */
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    /* rdevmajor + space/null padding: 8 bytes */
+    '0','0','0','0','0','0', ' ','\0',
+    /* rdevminor + space/null padding: 8 bytes */
+    '0','0','0','0','0','0', ' ','\0',
+    /* Prefix: 155 bytes */
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,
+    /* Padding: 12 bytes */
+    0,0,0,0,0,0,0,0, 0,0,0,0
 };
 
 @implementation DCTar
@@ -167,7 +167,6 @@ static const char template_header[] = {
     NSFileManager *manager = [NSFileManager defaultManager];
     if([manager fileExistsAtPath:filePath]) {
         NSDictionary *attributes = [manager attributesOfItemAtPath:filePath error:nil];
-        unsigned long long size = [attributes[NSFileSize] longLongValue];
         NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:filePath];
         
         if([filePath hasSuffix:@".gz"]) {
@@ -177,7 +176,6 @@ static const char template_header[] = {
             if([self fileInflate:fileHandle isGzip:YES toPath:tarPath]) {
                 [fileHandle closeFile];
                 attributes = [manager attributesOfItemAtPath:tarPath error:nil];
-                size = [attributes[NSFileSize] longLongValue];
                 fileHandle = [NSFileHandle fileHandleForReadingAtPath:tarPath];
                 filePath = tarPath;
             }
@@ -211,14 +209,14 @@ static const char template_header[] = {
         for(NSString *file in [fileManager enumeratorAtPath:tarFilePath]) {
             BOOL isDir = NO;
             [fileManager fileExistsAtPath:[tarFilePath stringByAppendingPathComponent:file] isDirectory:&isDir];
-            NSData *tarContent = [self binaryEncodeDataForPath:file inDirectory:tarFilePath isDirectory:isDir];
-            [fileHandle writeData:tarContent];
+            [self chunkedWriteDataFromPath:file inDirectory:tarFilePath isDirectory:isDir toFileHandle: fileHandle];
         }
         //Append two empty blocks to indicate end
         char block[TAR_BLOCK_SIZE*2];
         memset(&block, '\0', TAR_BLOCK_SIZE*2);
         [fileHandle writeData:[NSData dataWithBytes:block length:TAR_BLOCK_SIZE*2]];
         [fileHandle closeFile];
+        free(block);
         return YES;
     }
     if(error)
@@ -370,7 +368,9 @@ static const char template_header[] = {
     memset(&nameBytes, '\0', TAR_NAME_SIZE + 1); // Fill byte array with nul char
     memcpy(&nameBytes, [self dataForObject:object inRange:NSMakeRange((uInt)offset + TAR_NAME_POSITION, TAR_NAME_SIZE)
                                 orLocation:offset + TAR_NAME_POSITION andLength:TAR_NAME_SIZE].bytes, TAR_NAME_SIZE);
-    return [NSString stringWithCString:nameBytes encoding:NSASCIIStringEncoding];
+    NSString *name = [NSString stringWithCString:nameBytes encoding:NSASCIIStringEncoding];
+    free(nameBytes);
+    return name;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 + (unsigned long long)sizeForObject:(id)object atOffset:(unsigned long long)offset
@@ -380,7 +380,9 @@ static const char template_header[] = {
     memset(&sizeBytes, '\0', TAR_SIZE_SIZE + 1); // Fill byte array with nul char
     memcpy(&sizeBytes, [self dataForObject:object inRange:NSMakeRange((uInt)offset + TAR_SIZE_POSITION, TAR_SIZE_SIZE)
                                 orLocation:offset + TAR_SIZE_POSITION andLength:TAR_SIZE_SIZE].bytes, TAR_SIZE_SIZE);
-    return strtol(sizeBytes, NULL, 8); // Size is an octal number, convert to decimal
+    unsigned long long size = strtol(sizeBytes, NULL, 8); // Size is an octal number, convert to decimal
+    free(sizeBytes);
+    return size;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 +(void)writeFileDataForObject:(id)object atLocation:(unsigned long long)location withLength:(unsigned long long)length atPath:(NSString *)path
@@ -420,27 +422,24 @@ static const char template_header[] = {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //tar stuff
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-+ (NSData*)binaryEncodeDataForPath:(NSString *) path inDirectory:(NSString *)basepath  isDirectory:(BOOL) isDirectory{
-    
-    NSMutableData *tarData;
-    char block[TAR_BLOCK_SIZE];
++ (void)chunkedWriteDataFromPath:(NSString *) path inDirectory:(NSString *)basepath  isDirectory:(BOOL) isDirectory toFileHandle:(NSFileHandle *)outputFileHandle {
     
     if(isDirectory) {
         path = [path stringByAppendingString:@"/"];
     }
+    
     //write header
-    [self writeHeader:block forPath:path withBasePath:basepath isDirectory:isDirectory];
-    tarData = [NSMutableData dataWithBytes:block length:TAR_BLOCK_SIZE];
+    [self writeHeaderForPath:path withBasePath:basepath isDirectory:isDirectory toFileHandle:outputFileHandle];
     
     //write data
     if(!isDirectory) {
-        [self writeDataFromPath: [basepath stringByAppendingPathComponent:path] toData:tarData];
+        [self chunkedWriteDataFromPath: [basepath stringByAppendingPathComponent:path] toFileHandle: outputFileHandle];
     }
-    return tarData;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-+(void)writeHeader:(char*)buffer forPath:(NSString*)path withBasePath:(NSString*)basePath isDirectory:(BOOL)isDirectory {
++(void)writeHeaderForPath:(NSString*)path withBasePath:(NSString*)basePath isDirectory:(BOOL)isDirectory toFileHandle:(NSFileHandle *)outputFileHandle  {
     
+    char buffer[TAR_BLOCK_SIZE];
     memcpy(buffer,&template_header, TAR_BLOCK_SIZE);
     NSError *error = nil;
     NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:[basePath stringByAppendingPathComponent:path] error:&error];
@@ -463,31 +462,31 @@ static const char template_header[] = {
     format_number(permissions & 07777, buffer+USTAR_mode_offset, USTAR_mode_size, USTAR_mode_max_size, 0);
     format_number(ownerId,
                   buffer + USTAR_uid_offset, USTAR_uid_size, USTAR_uid_max_size, 0);
-	format_number(groupId,
+    format_number(groupId,
                   buffer + USTAR_gid_offset, USTAR_gid_size, USTAR_gid_max_size, 0);
     
-	format_number(fileSize, buffer + USTAR_size_offset, USTAR_size_size, USTAR_size_max_size, 0);
+    format_number(fileSize, buffer + USTAR_size_offset, USTAR_size_size, USTAR_size_max_size, 0);
     
-	format_number([modificationDate timeIntervalSince1970],
+    format_number([modificationDate timeIntervalSince1970],
                   buffer + USTAR_mtime_offset, USTAR_mtime_size, USTAR_mtime_max_size, 0);
     
     unsigned long nameLength = strlen(nameChar);
     if (nameLength <= USTAR_name_size)
-		memcpy(buffer + USTAR_name_offset, nameChar, nameLength);
-	else {
-		/* Store in two pieces, splitting at a '/'. */
-		const char *p = strchr(nameChar + nameLength - USTAR_name_size - 1, '/');
-		/*
-		 * Look for the next '/' if we chose the first character
-		 * as the separator.  (ustar format doesn't permit
-		 * an empty prefix.)
-		 */
-		if (p == nameChar)
-			p = strchr(p + 1, '/');
+        memcpy(buffer + USTAR_name_offset, nameChar, nameLength);
+    else {
+        /* Store in two pieces, splitting at a '/'. */
+        const char *p = strchr(nameChar + nameLength - USTAR_name_size - 1, '/');
+        /*
+         * Look for the next '/' if we chose the first character
+         * as the separator.  (ustar format doesn't permit
+         * an empty prefix.)
+         */
+        if (p == nameChar)
+            p = strchr(p + 1, '/');
         memcpy(buffer + USTAR_prefix_offset, nameChar, p - nameChar);
-		memcpy(buffer + USTAR_name_offset, p + 1,
+        memcpy(buffer + USTAR_name_offset, p + 1,
                nameChar + nameLength - p - 1);
-	}
+    }
     
     memcpy(buffer+USTAR_uname_offset,unameChar,USTAR_uname_size);
     memcpy(buffer+USTAR_gname_offset,gnameChar,USTAR_gname_size);
@@ -500,19 +499,38 @@ static const char template_header[] = {
     //Checksum
     int checksum = 0;
     for (int i = 0; i < TAR_BLOCK_SIZE; i++)
-		checksum += 255 & (unsigned int)buffer[i];
-	buffer[USTAR_checksum_offset + 6] = '\0';
-	format_octal(checksum, buffer + USTAR_checksum_offset, 6);
+        checksum += 255 & (unsigned int)buffer[i];
+    buffer[USTAR_checksum_offset + 6] = '\0';
+    format_octal(checksum, buffer + USTAR_checksum_offset, 6);
+    [outputFileHandle writeData:[NSData dataWithBytes:buffer length:TAR_BLOCK_SIZE]];
+    
+    free(buffer);
+    free(nameChar);
+    free(unameChar);
+    free(gnameChar);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-+(void)writeDataFromPath:(NSString *)path toData:(NSMutableData*)data {
-    NSData *content = [NSData dataWithContentsOfFile:path];
-    NSUInteger contentSize = [content length];
-    unsigned long padding =  (TAR_BLOCK_SIZE - (contentSize % TAR_BLOCK_SIZE)) % TAR_BLOCK_SIZE ;
-    char buffer[padding];
-    memset(&buffer, '\0', padding);
-    [data appendData:content];
-    [data appendBytes:buffer length:padding];
++(void)chunkedWriteDataFromPath:(NSString *)path toFileHandle:(NSFileHandle*) outputFileHandle {
+    NSFileHandle *sourceFile = [NSFileHandle fileHandleForReadingAtPath:path];
+    
+    NSUInteger fileLength = [sourceFile seekToEndOfFile];
+    NSUInteger offset = 0;
+    [sourceFile seekToFileOffset:0];
+    
+    while(offset < fileLength) {
+        NSData *chunk = [sourceFile readDataOfLength:TAR_BLOCK_SIZE];
+        NSUInteger chunkSize = [chunk length];
+        [outputFileHandle writeData:chunk];
+        
+        unsigned long padding =  (TAR_BLOCK_SIZE - (chunkSize % TAR_BLOCK_SIZE)) % TAR_BLOCK_SIZE;
+        if(padding > 0) {
+            char buffer[padding];
+            memset(&buffer, '\0', padding);
+            [outputFileHandle writeData:[NSData dataWithBytes:buffer length:TAR_BLOCK_SIZE*2]];
+            free(buffer);
+        }
+        offset += chunkSize;
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 + (void)writeString:(NSString*)string toChar:(char*)charArray withLength:(NSInteger)size
@@ -529,73 +547,73 @@ static const char template_header[] = {
 //Format a number into a field, with some intelligence.
 static int format_number(int64_t v, char *p, int s, int maxsize, int strict)
 {
-	int64_t limit;
+    int64_t limit;
     
-	limit = ((int64_t)1 << (s*3));
+    limit = ((int64_t)1 << (s*3));
     
-	/* "Strict" only permits octal values with proper termination. */
-	if (strict)
-		return (format_octal(v, p, s));
+    /* "Strict" only permits octal values with proper termination. */
+    if (strict)
+        return (format_octal(v, p, s));
     
-	/*
-	 * In non-strict mode, we allow the number to overwrite one or
-	 * more bytes of the field termination.  Even old tar
-	 * implementations should be able to handle this with no
-	 * problem.
-	 */
-	if (v >= 0) {
-		while (s <= maxsize) {
-			if (v < limit)
-				return (format_octal(v, p, s));
-			s++;
-			limit <<= 3;
-		}
-	}
+    /*
+     * In non-strict mode, we allow the number to overwrite one or
+     * more bytes of the field termination.  Even old tar
+     * implementations should be able to handle this with no
+     * problem.
+     */
+    if (v >= 0) {
+        while (s <= maxsize) {
+            if (v < limit)
+                return (format_octal(v, p, s));
+            s++;
+            limit <<= 3;
+        }
+    }
     
-	/* Base-256 can handle any number, positive or negative. */
-	return (format_256(v, p, maxsize));
+    /* Base-256 can handle any number, positive or negative. */
+    return (format_256(v, p, maxsize));
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //Format a number into the specified field using base-256.
 static int format_256(int64_t v, char *p, int s)
 {
-	p += s;
-	while (s-- > 0) {
-		*--p = (char)(v & 0xff);
-		v >>= 8;
-	}
-	*p |= 0x80; /* Set the base-256 marker bit. */
-	return (0);
+    p += s;
+    while (s-- > 0) {
+        *--p = (char)(v & 0xff);
+        v >>= 8;
+    }
+    *p |= 0x80; /* Set the base-256 marker bit. */
+    return (0);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //Format a number into the specified field.
 static int format_octal(int64_t v, char *p, int s)
 {
-	int len;
+    int len;
     
-	len = s;
+    len = s;
     
-	/* Octal values can't be negative, so use 0. */
-	if (v < 0) {
-		while (len-- > 0)
-			*p++ = '0';
-		return (-1);
-	}
+    /* Octal values can't be negative, so use 0. */
+    if (v < 0) {
+        while (len-- > 0)
+            *p++ = '0';
+        return (-1);
+    }
     
-	p += s;		/* Start at the end and work backwards. */
-	while (s-- > 0) {
-		*--p = (char)('0' + (v & 7));
-		v >>= 3;
-	}
+    p += s;		/* Start at the end and work backwards. */
+    while (s-- > 0) {
+        *--p = (char)('0' + (v & 7));
+        v >>= 3;
+    }
     
-	if (v == 0)
-		return (0);
+    if (v == 0)
+        return (0);
     
-	/* If it overflowed, fill field with max value. */
-	while (len-- > 0)
-		*p++ = '7';
+    /* If it overflowed, fill field with max value. */
+    while (len-- > 0)
+        *p++ = '7';
     
-	return (-1);
+    return (-1);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //compression stuff
